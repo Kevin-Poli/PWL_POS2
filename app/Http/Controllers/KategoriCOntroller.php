54af96faf319f6a56c41f10240c6825ad2a9788d
Kevin-Poli <khususmaen4154@gmail.com>
2024-09-18 16:26:24 +0700

    Update KategoriCOntroller.php

--- app/Http/Controllers/KategoriCOntroller.php
+++ app/Http/Controllers/KategoriCOntroller.php
@@ -19,8 +19,8 @@ class KategoriCOntroller extends Controller
         // $row = DB::table('m_kategori') -> where ('kategori_kode', 'SNK') ->update(['kategori_nama'=>'camilan']);
         // return 'Update Data Telah BErhasil. Jumlah dat ayang di update : '.$row.' baris';
 
-        // $row = DB::table('m_kategori') -> where('kategori_kode','SNK') -> delete();
-        // return 'Delete Data Telah BErhasil. Jumlah datayang di Hapus : '.$row.' baris';
+        $row = DB::table('m_kategori') -> where('kategori_kode','SNK') -> delete();
+        return 'Delete Data Telah BErhasil. Jumlah datayang di Hapus : '.$row.' baris';
 
         $data = DB::table('m_kategori') -> get();
         return view('kategori',['data' => $data]);
