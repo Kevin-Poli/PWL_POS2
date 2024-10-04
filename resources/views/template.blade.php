2e1bff9666403d31784508cedef59d4760e0c89c
Kevin-Poli <khususmaen4154@gmail.com>
2024-10-04 14:40:45 +0700

    JS6 Praktikum 1

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -10,6 +10,12 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
   <link rel="stylesheet" href="{{asset('adminlte/pulgins/fontawesome-free/css/all.min.css')}}">
+  <!-- Datatables-->
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('adminlte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+   <!-- SweetAlert2-->
+   <link rel="stylesheet" href="{{assets ('dist/css/adminlte.min.css') }}">
   <!-- Theme style -->
   <link rel="stylesheet" href="{{asset('adminlte/dist/css/adminlte.min.css')}}">
 </head>
@@ -47,11 +53,16 @@
   </div>
   <!-- /.content-wrapper -->
   @yield('layouts.footer')
- 
+
 <!-- jQuery -->
 <script src="{{assets('adminlte/plugins/jquery/jquery.min.js')}}"></script>
 <!-- Bootstrap 4 -->
 <script src="{{assets('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- jquery-validation -->
+<script src="{{assets('plugins/jquery-validation/jquery.validate.min.js')}}"></script>
+<script src="{{assets('plugins/jquery-validation/additional-methods.min.js')}}"></script>
+<!-- SweetAlert2 -->
+<script src="{{assets('plugins/sweetalert2/sweetalert2.min.js')}}"></script>
 <!-- AdminLTE App -->
 <script src="{{assets('adminlte/dist/js/adminlte.min.js')}}"></script>
 <!-- AdminLTE for demo purposes -->
