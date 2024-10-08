f81d386315a6196d1bd01664f1887f8a72042e03
Kevin-Poli <khususmaen4154@gmail.com>
2024-10-08 14:49:12 +0700

    JS6

--- resources/views/level/create_ajax.blade.php
+++ resources/views/level/create_ajax.blade.php
@@ -1,22 +1,21 @@
-<form action="{{ url('/kategori/ajax') }}" method="POST" id="form-tambah">
+<form action="{{ url('/level/ajax') }}" method="POST" id="form-tambah">
     @csrf
     <div id="modal-master" class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Tambah Data Kategori</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
-                        aria-hidden="true">&times;</span></button>
+                <h5 class="modal-title" id="exampleModalLabel">Tambah Data level</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
             </div>
             <div class="modal-body">
                 <div class="form-group">
-                    <label>Kode</label>
-                    <input value="" type="text" name="kategori_kode" id="kategori_kode" class="form-control" required>
-                    <small id="error-kategori_kode" class="error-text form-text text-danger"></small>
+                    <label>Level Kode</label>
+                    <input value="" type="text" name="level_kode" id="level_kode" class="form-control" required>
+                    <small id="error-level_kode" class="error-text form-text text-danger"></small>
                 </div>
                 <div class="form-group">
-                    <label>Nama</label>
-                    <input value="" type="text" name="kategori_nama" id="kategori_nama" class="form-control" required>
-                    <small id="error-kategori_nama" class="error-text form-text text-danger"></small>
+                    <label>Nama Level</label>
+                    <input value="" type="text" name="level_nama" id="level_nama" class="form-control" required>
+                    <small id="error-level_nama" class="error-text form-text text-danger"></small>
                 </div>
             </div>
             <div class="modal-footer">
@@ -30,12 +29,14 @@
     $(document).ready(function() {
         $("#form-tambah").validate({
             rules: {
-                kode: {
+                level_kode: {
                     required: true,
-                    maxlength: 10
+                    minlength: 3,
+                    maxlength: 20
                 },
-                nama: {
+                level_nama: {
                     required: true,
+                    minlength: 3,
                     maxlength: 100
                 }
             },
@@ -52,7 +53,7 @@
                                 title: 'Berhasil',
                                 text: response.message
                             });
-                            dataKategori.ajax.reload();
+                            dataLevel.ajax.reload();
                         } else {
                             $('.error-text').text('');
                             $.each(response.msgField, function(prefix, val) {
